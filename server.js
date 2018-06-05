17e99288f1348fe3d9c0c91ff2a0f237a8d9f8aa
Shohei Nakagawa <hukusuke1007@gmail.com>
2018-06-05 21:05:25 +0900

    u

--- server.js
+++ server.js
@@ -1,36 +1,29 @@
 //
-// Server starter/entry file.
-// Based on: https://github.com/clarkdo/create-nuxt-app/blob/master/template/server/index-express.js
-//
+// Note: Depending on what nuxt version you are running and various other factors, this may change over time.
+// Best to reference the latest version in nuxt's examples which I pulled this from:
+// https://github.com/nuxt/nuxt.js/blob/dev/examples/custom-server/server.js
+// 
 
-const express = require('express')
+const app = require('express')()
 const { Nuxt, Builder } = require('nuxt')
-const app = express()
+
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
-
-app.set('port', port)
 
 // Import and set Nuxt.js options
 let config = require('./nuxt.config.js')
 config.dev = !(process.env.NODE_ENV === 'production')
 
-async function start () {
-  // Init Nuxt.js
-  const nuxt = new Nuxt(config)
+const nuxt = new Nuxt(config)
 
-  // Start build process in dev mode
-  if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
-  }
-
-  // Give nuxt middleware to express
-  app.use(nuxt.render)
-
-  // Start express server
-  app.listen(port, host)
-  console.log('Server listening on http://' + host + ':' + port) // eslint-disable-line no-console
+// Start build process in dev mode
+if (config.dev) {
+  const builder = new Builder(nuxt)
+  builder.build()
 }
 
-start()
+// Give nuxt middleware to express
+app.use(nuxt.render)
+
+// Start express server
+app.listen(port, host)

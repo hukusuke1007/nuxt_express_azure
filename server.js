3bdc1e62914efb8963d6ab8e72c39d08cdaac53c
Shohei Nakagawa <hukusuke1007@gmail.com>
2018-06-05 20:26:11 +0900

    u

---
+++ server.js
@@ -0,0 +1,36 @@
+//
+// Server starter/entry file.
+// Based on: https://github.com/clarkdo/create-nuxt-app/blob/master/template/server/index-express.js
+//
+
+const express = require('express')
+const { Nuxt, Builder } = require('nuxt')
+const app = express()
+const host = process.env.HOST || '127.0.0.1'
+const port = process.env.PORT || 3000
+
+app.set('port', port)
+
+// Import and set Nuxt.js options
+let config = require('./nuxt.config.js')
+config.dev = !(process.env.NODE_ENV === 'production')
+
+async function start () {
+  // Init Nuxt.js
+  const nuxt = new Nuxt(config)
+
+  // Start build process in dev mode
+  if (config.dev) {
+    const builder = new Builder(nuxt)
+    await builder.build()
+  }
+
+  // Give nuxt middleware to express
+  app.use(nuxt.render)
+
+  // Start express server
+  app.listen(port, host)
+  console.log('Server listening on http://' + host + ':' + port) // eslint-disable-line no-console
+}
+
+start()
